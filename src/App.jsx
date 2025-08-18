120dd96ff612d4f5f0d8b73836ee1d698603cfb1
Simone Conti <simonecontisid@gmail.com>
2025-08-18 12:43:51 +0200

    use of if for conditional display
    Signed-off-by: Simone Conti <simonecontisid@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,32 @@ function App() {
     <>
 
     <div className="grid grid-cols-4 gap-10 p-4">
-    <Card title="Tokyo" imgURL="https://images.pexels.com/photos/3779837/pexels-photo-3779837.jpeg">
-    This is a description of the card of Tokyo.</Card>
-    <Card title="New York" imgURL="https://images.pexels.com/photos/1796505/pexels-photo-1796505.jpeg">
-    This is a description of the card of New York</Card>
+    <Card 
+    isVisited={false}
+    title="Tokyo" 
+    imgURL="https://images.pexels.com/photos/3779837/pexels-photo-3779837.jpeg">
+    This is a description of the card of Tokyo.
+    </Card>
+    
+    <Card 
+    isVisited={true}
+    title="New York" 
+    imgURL="https://images.pexels.com/photos/1796505/pexels-photo-1796505.jpeg">
+    This is a description of the card of New York
+    </Card>
 
-    <Card title="Rome" imgURL="https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg">
-    This is a description of the card of Rome</Card>
-    <Card title="Paris" imgURL="https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg">
-    This is a description of the card of Paris</Card>
+    <Card 
+    isVisited={true}
+    title="Rome" 
+    imgURL="https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg">
+    This is a description of the card of Rome
+    </Card>
+    <Card 
+    isVisited={true}
+    title="Paris" 
+    imgURL="https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg">
+    This is a description of the card of Paris
+    </Card>
 </div>
       
      
