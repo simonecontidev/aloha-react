87e948760d842fef275a3cd71c39d101920b27b0
Simone Conti <simonecontisid@gmail.com>
2025-08-18 12:32:12 +0200

    use of children and props
    Signed-off-by: Simone Conti <simonecontisid@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,15 @@ function App() {
     <>
 
     <div className="grid grid-cols-4 gap-10 p-4">
-    <Card title="Tokyo" imgURL="https://images.pexels.com/photos/3779837/pexels-photo-3779837.jpeg"></Card>
-    <Card title="New York" imgURL="https://images.pexels.com/photos/1796505/pexels-photo-1796505.jpeg"></Card>
+    <Card title="Tokyo" imgURL="https://images.pexels.com/photos/3779837/pexels-photo-3779837.jpeg">
+    This is a description of the card of Tokyo.</Card>
+    <Card title="New York" imgURL="https://images.pexels.com/photos/1796505/pexels-photo-1796505.jpeg">
+    This is a description of the card of New York</Card>
 
-    <Card title="Rome" imgURL="https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg"></Card>
-    <Card title="Paris" imgURL="https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg"></Card>
+    <Card title="Rome" imgURL="https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg">
+    This is a description of the card of Rome</Card>
+    <Card title="Paris" imgURL="https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg">
+    This is a description of the card of Paris</Card>
 </div>
       
      
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,11 @@
-function Card({title, imgURL}) {
-
-    const description = "This is a description of the card.";
+function Card({title, imgURL, children}) {
 
   return (
     <div className="rounded-md bg-zinc-950 shadow-lg p-4 flex flex-col items-center">
       <img src={imgURL} alt="Random" />
       <div className="flex flex-col items-center p-4">
         <h2 className="text-2xl text-white font-bold">{title}</h2>
-        <p className="text-gray-500">{description}</p>
+        <p className="text-gray-500">{children}</p>
       </div>
     </div>
   );
