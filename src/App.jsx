e259d1ab779f437aeb1584c035358d0e0933a367
Simone Conti <simonecontisid@gmail.com>
2025-08-18 12:14:18 +0200

    dynamic component card
    Signed-off-by: Simone Conti <simonecontisid@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,16 @@ function App() {
 
   return (
     <>
+
+    <div className="grid grid-cols-4 gap-10 p-4">
     <Card></Card>
-      <Navbar />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <Card></Card>
+
+    <Card></Card>
+    <Card></Card>
+</div>
+      
+     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,9 +30,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
-function Card(){
-    return(
-        <div>
-            <h4>Ciao sono una card</h4>
-        </div>
-    )
+function Card() {
+
+    const title = "Tokyo";
+    const description = "This is a description of the card for Tokyo.";
+    const imgURL = "https://picsum.photos/200/300";
+
+
+  return (
+    <div className="rounded-md bg-zinc-950 shadow-lg p-4 flex flex-col items-center">
+      <img src={imgURL} alt="Random" />
+      <div className="flex flex-col items-center p-4">
+        <h2 className="text-2xl text-white font-bold">{title}</h2>
+        <p className="text-gray-500">{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
