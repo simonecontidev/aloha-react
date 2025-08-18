e43d95be8b583b431cee18a0e6c366867ed9a106
Simone Conti <simonecontisid@gmail.com>
2025-08-18 10:27:05 +0200

    component navbar
    Signed-off-by: Simone Conti <simonecontisid@gmail.com>

---
+++ src/components/Link.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+function Link(){
+    return(
+    <div>
+        <a href="#home">Home</a>
+    </div>
+    ) 
+}
+
+export default Link;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+import Link from './Link.jsx';
+
+function Navbar(){
+    return (
+        <nav>
+        <ul>
+            <li><Link></Link><p>hola</p></li>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+        </ul>
+        </nav>
+    );
+}
+export default Navbar;
