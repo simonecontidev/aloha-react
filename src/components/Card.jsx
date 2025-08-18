626384f305ebb54cca0358bbab707368208fa3b2
Simone Conti <simonecontisid@gmail.com>
2025-08-18 12:47:24 +0200

    render conditional
    Signed-off-by: Simone Conti <simonecontisid@gmail.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,13 @@
 function Card({title, imgURL, isVisited, children}) {
-  if(isVisited) {
-    return (
-    <div className="rounded-md bg-zinc-950 shadow-lg p-4 flex flex-col items-center">
-      <img src={imgURL} alt="Random" />
-      <div className="flex flex-col items-center p-4">
-        <h2 className="text-2xl text-white font-bold">{title}</h2>
-        <p className="text-gray-500">{children}</p>
-        <span>&#10003; visited</span>
-      </div>
-    </div>
-  );
-  } 
 return (
     <div className="rounded-md bg-zinc-950 shadow-lg p-4 flex flex-col items-center">
       <img src={imgURL} alt="Random" />
       <div className="flex flex-col items-center p-4">
         <h2 className="text-2xl text-white font-bold">{title}</h2>
         <p className="text-gray-500">{children}</p>
-        <span>&#10005; not visited</span>
+        {isVisited ?
+          <span className="text-green-500 font-bold">Visited</span> :  <span>&#10005; not visited</span>}
+      
 
       </div>
     </div>
